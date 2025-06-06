4d1fceb8abb43f419295a8334a52598332966536
manoj2990 <manojmeena2990@gmail.com>
2025-06-06 11:10:09 +0530

    correction

--- src/zodSchemas/department.schema.ts
+++ src/zodSchemas/department.schema.ts
@@ -2,8 +2,8 @@ import { z } from 'zod';
 
 // Base department schema
 export const departmentSchema = z.object({
-    name: z.string().min(1, "Department name is required").trim(),
     organizationId: z.string().min(1, "Organization ID is required"),
+    name: z.string().min(1, "Department name is required").trim(),
     description: z.string().optional(),
     courses: z.array(z.string()).optional(),
     adminId:z.string()
@@ -16,8 +16,12 @@ export const departmentSchema = z.object({
 
 // Department update schema
 export const departmentUpdateSchema = z.object({
+    organizationId: z.string().min(1, "Organization ID is required"),
    departmentId: z.string().min(1, "Department ID is required"),
-    body: departmentSchema.partial()
+    name: z.string().min(1, "Department name is required").trim().optional(),
+    description: z.string().optional(),
+    courses: z.array(z.string()).optional(),
+    adminId:z.string()
 });
 
 // // Department ID schema
